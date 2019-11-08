168253b3a5d5e6c9c6de9745922e94e10c471490
Michael Koek <michaelkoek@saxum.nl>
2019-11-08 19:12:10 +0100

    Added: ImageSlider package to use for the carousel

--- attracty/components/UI/CardImage.tsx
+++ attracty/components/UI/CardImage.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
-import { Dimensions } from 'react-native';
+import { Dimensions, View } from 'react-native';
 import styled from 'styled-components/native';
+import ImageSlider from 'react-native-image-slider';
 
 import { ICandidateProps } from '../../types/candidates';
 
 const CardImage: React.FC<ICandidateProps> = ({ photos }) => {
+    const images: string[] = photos.map(photo => photo.url);
+
     return (
         <ImageContainer>
-            <ProfileImage resizeMode="cover" source={{ uri: photos[0].url }} />
+            {images.length > 1 ? (
+                <ImageSlider
+                    images={images}
+                    customSlide={({ index, item, style }) => (
+                        <View key={index} style={[style]}>
+                            <ProfileImage
+                                resizeMode="cover"
+                                source={{ uri: item }}
+                            />
+                        </View>
+                    )}
+                />
+            ) : (
+                <ProfileImage resizeMode="cover" source={{ uri: images[0] }} />
+            )}
         </ImageContainer>
     );
 };

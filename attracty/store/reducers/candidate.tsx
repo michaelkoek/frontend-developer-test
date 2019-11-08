b2ca3067620c1825a4c0d6435c6a7176f8c46c00
Michael Koek <michaelkoek@saxum.nl>
2019-11-08 23:25:45 +0100

    Update and Added: Reducer handles the MATCH and DISLIKE actions. Both choices updates the availbleCandidates array

--- attracty/store/reducers/candidate.tsx
+++ attracty/store/reducers/candidate.tsx
@@ -1,15 +1,51 @@
-import { CANDIDATES, CandidateTypes } from '../types';
+import { CANDIDATES } from '../types';
+import { ICandidateProps } from '../../types/candidates';
 
 const initialState = {
     availableCandidates: [],
+    matchedCandidates: [],
 };
 
 const candidateReducer = (state = initialState, action) => {
+    const removeFromFetchedList = (personId: string): ICandidateProps[] => {
+        const findPerson = state.availableCandidates.findIndex(
+            candidate => candidate.id === personId
+        );
+
+        const removeFoundPerson = [...state.availableCandidates];
+        removeFoundPerson.splice(findPerson, 1);
+        return removeFoundPerson;
+    };
+
     switch (action.type) {
         case CANDIDATES.SET:
+            // if (state.availableCandidates.length === 0) {
             return {
+                ...state,
                 availableCandidates: action.candidates,
             };
+            // }
+            return {
+                ...state,
+            };
+
+        case CANDIDATES.MATCH:
+            const matched = state.availableCandidates.find(
+                candidate => candidate.id === action.candidateId
+            );
+
+            return {
+                ...state,
+                availableCandidates: removeFromFetchedList(action.candidateId),
+                matchedCandidates: [...state.matchedCandidates, matched],
+            };
+
+        case CANDIDATES.DISLIKE:
+            return {
+                ...state,
+                availableCandidates: removeFromFetchedList(action.candidateId),
+            };
+
         default:
             return state;
     }
